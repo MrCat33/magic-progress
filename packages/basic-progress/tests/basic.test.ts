4caa94e595e780ebb15cc9a6a90a533fbabaaffd
MrCat <1025090504@qq.com>
2022-10-29 00:02:55 +0800

    fix:fix unit test

--- packages/basic-progress/tests/basic.test.ts
+++ packages/basic-progress/tests/basic.test.ts
@@ -1,5 +1,23 @@
-import { expect, test } from 'vitest'
+import { resolve } from 'node:path'
+import { describe, expect, it } from 'vitest'
+import glob from 'fast-glob'
+import { readFile } from 'fs/promises'
+import { transfrom } from '../src'
 
-test('basic', () => {
-  expect('foo').toBe('foo')
+describe('Vue', () => {
+  describe('fixtures', async () => {
+    const root = resolve(__dirname, '..')
+    const files = await glob('tests/fixtures/*.{vue,js,ts}', {
+      cwd: root,
+      onlyFiles: true,
+    })
+
+    for (const file of files) {
+      it(file.replace(/\\/g, '/'), async () => {
+        const filepath = resolve(root, file)
+        const code = transfrom(await readFile(filepath,'utf-8'))
+        expect(code).toMatchSnapshot()
+      })
+    }
+  })
 })
